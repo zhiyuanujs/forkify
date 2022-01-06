4433815ecb5275e18250ed4be401f12b678f19ec
Xu <zhiyuanoutlook.com>
2022-01-06 23:06:31 +0100

    MVC structure for the basic forkify feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,26 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) loading recipe
+    await model.loadRecipe(id);
+
+    // 2) rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+const init = function () {
+  recipeView.addHanderRender(controlRecipes);
+};
+init();
